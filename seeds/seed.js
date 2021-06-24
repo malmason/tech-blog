6b03cb458eb0420f9270192646802536e40b9932
Malcolm Mason <79657911+malmason@users.noreply.github.com>
2021-06-24 11:19:08 -0400

    Created seed files and sample data for all models

---
+++ seeds/seed.js
@@ -0,0 +1,34 @@
+const sequelize = require('../config/connection');
+const { User, Blogpost, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const blogData = require('./blogData.json');
+const commentData = require('./commentData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const blog of blogData) {
+    await Blogpost.create({
+      ...blog,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+      blog_id: users[Math.floor(Math.random() * users.length - 1)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
