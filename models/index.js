d354b9a539a057c7364f1991b2bd13981cd8a4f2
Malcolm Mason <79657911+malmason@users.noreply.github.com>
2021-06-24 10:55:19 -0400

    Completed folder structure, models, config, and db setup

---
+++ models/index.js
@@ -0,0 +1,44 @@
+// import models
+const { json } = require('sequelize/types');
+const Blogpost = require('./Blogpost');
+const Comment = require('./Comment');
+const User = require('./User');
+
+// Comments belongs to Blogpost
+Comment.belongsTo(Blogpost, {
+  foreignKey: 'blog_id',
+});
+
+// Posts have many comments
+Blogpost.hasMany(Comment, {
+  foreignKey: 'blog_id',
+  onDelete: 'CASCADE',
+});
+
+// User has many posts
+User.hasMany(Blogpost, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+// Posts belong to 1 user
+Blogpost.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+// User has many comments
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+// Comment belongs to 1 user
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+module.exports = {
+  Blogpost,
+  Comment, 
+  User,
+};
